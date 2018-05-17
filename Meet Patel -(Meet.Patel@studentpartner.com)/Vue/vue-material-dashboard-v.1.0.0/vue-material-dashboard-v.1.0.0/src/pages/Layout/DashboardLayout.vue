c2882da3a7a0c077073423f6704717451929b1e6
Meet Patel <meetcpatel906@gmail.com>
2018-05-17 16:08:15 +0530

    main js

--- Meet Patel -(Meet.Patel@studentpartner.com)/Vue/vue-material-dashboard-v.1.0.0/vue-material-dashboard-v.1.0.0/src/pages/Layout/DashboardLayout.vue
+++ Meet Patel -(Meet.Patel@studentpartner.com)/Vue/vue-material-dashboard-v.1.0.0/vue-material-dashboard-v.1.0.0/src/pages/Layout/DashboardLayout.vue
@@ -6,7 +6,7 @@
       <mobile-menu slot="content"></mobile-menu>
       <sidebar-link to="/dashboard">
         <md-icon>dashboard</md-icon>
-        <p>Toilet Id</p>
+        <p>Dashboard</p>
       </sidebar-link>
     
       
@@ -17,10 +17,65 @@
         <p>Maps</p>
       </sidebar-link>
 
+      
       <sidebar-link to="/maps">
         <md-icon>location_on</md-icon>
-        <p>Maps</p>
-        
+        <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex xs12 lg6>
+        <v-menu
+          ref="menu1"
+          :close-on-content-click="false"
+          v-model="menu1"
+          :nudge-right="40"
+          lazy
+          transition="scale-transition"
+          offset-y
+          full-width
+          max-width="290px"
+          min-width="290px"
+        >
+          <v-text-field
+            slot="activator"
+            v-model="dateFormatted"
+            label="Date"
+            hint="MM/DD/YYYY format"
+            persistent-hint
+            prepend-icon="event"
+            @blur="date = parseDate(dateFormatted)"
+          ></v-text-field>
+          <v-date-picker v-model="date" no-title @input="menu1 = false"></v-date-picker>
+        </v-menu>
+        <p>Date in ISO format: <strong>{{ date }}</strong></p>
+      </v-flex>
+
+      <v-flex xs12 lg6>
+        <v-menu
+          :close-on-content-click="false"
+          v-model="menu2"
+          :nudge-right="40"
+          lazy
+          transition="scale-transition"
+          offset-y
+          full-width
+          max-width="290px"
+          min-width="290px"
+        >
+          <v-text-field
+            slot="activator"
+            v-model="computedDateFormatted"
+            label="Date (read only text field)"
+            hint="MM/DD/YYYY format"
+            persistent-hint
+            prepend-icon="event"
+            readonly
+          ></v-text-field>
+          <v-date-picker v-model="date" no-title @input="menu2 = false"></v-date-picker>
+        </v-menu>
+        <p>Date in ISO format: <strong>{{ date }}</strong></p>
+      </v-flex>
+    </v-layout>
+  </v-container>
       </sidebar-link>
     
     </side-bar>
@@ -44,6 +99,8 @@ import TopNavbar from './TopNavbar.vue'
 import ContentFooter from './ContentFooter.vue'
 import DashboardContent from './Content.vue'
 import MobileMenu from '@/pages/Layout/MobileMenu.vue'
+//@import url("//unpkg.com/iview/dist/styles/iview.css");
+
 
 export default {
   components: {
