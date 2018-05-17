98ad79e81d48e319f2615e644c454e6d6502067b
Meet Patel <meetcpatel906@gmail.com>
2018-05-17 19:14:13 +0530

    Vue Added

--- Meet Patel -(Meet.Patel@studentpartner.com)/UI/UI/src/components/AppHome.vue
+++ Meet Patel -(Meet.Patel@studentpartner.com)/UI/UI/src/components/AppHome.vue
@@ -1,14 +1,14 @@
 <template>
     <app-toolbar></app-toolbar>
-    <app-footer></app-footer>
+  
 </template>
 
 <script>
 import AppFooter from './AppFooter'
 import AppToolbar from './AppToolbar '
 export default {
-    components:{AppFooter,AppToolbar}
+    
     }
-}
+
 </script>
 
